707aaa4922dc0e35b3a93e8c6b84d372d5482fa2
Petr Jindra <petr@gbol.cz>
2023-12-15 15:10:12 +0100

    readme and examples

--- examples/render_email.rs
+++ examples/render_email.rs
@@ -1,14 +1,54 @@
-use terarium::{Template, TerariumBuilder};
+use tera::Context;
+use terarium::{Template, TemplateGroupBuilder, TerariumBuilder};
 
 fn main() {
     let terarium = TerariumBuilder::<String>::default()
         .add_template(
-            "subject".to_owned(),
+            "greet_subject".to_owned(),
             Template::<String>::default()
                 .content_builder()
-                .add_content("Hello {username}".to_owned(), vec!["en".to_owned()])
-                .add_content("Nazdar {username}".to_owned(), vec!["cs".to_owned()])
+                .add_content("Greetings from {{sender}}".to_owned(), vec!["en".to_owned()])
+                .add_content("Pozdrav od {{sender}}".to_owned(), vec!["cs".to_owned()])
+                .build()
+        )
+        .add_template(
+            "greet_text".to_owned(),
+            Template::<String>::default()
+                .content_builder()
+                .add_content("Hello {{username}}".to_owned(), vec!["en".to_owned()])
+                .add_content("Nazdar {{username}}".to_owned(), vec!["cs".to_owned()])
+                .build()
+        )
+        .add_template(
+            "greet_html".to_owned(),
+            Template::<String>::default()
+                .content_builder()
+                .add_content("<p>Hello {{username}}</p>".to_owned(), vec!["en".to_owned()])
+                .add_content("<p>Nazdar {{username}}</p>".to_owned(), vec!["cs".to_owned()])
+                .build()
+        )
+        .add_group(
+            "greet_email".to_string(),
+            TemplateGroupBuilder::default()
+                .add_member("subject".to_owned(), "greet_subject".to_owned())
+                .add_member("text".to_owned(), "greet_text".to_owned())
+                .add_member("html".to_owned(), "greet_html".to_owned())
                 .build()
         )
         .build().unwrap();
+
+    let mut ctx = Context::new();
+    ctx.insert("sender", "Jara Cimrman");
+    ctx.insert("username", "Karel Capek");
+
+    let rendered_group_en = terarium.render_group(&ctx, "greet_email", "en", None).unwrap();
+    let rendered_group_cs = terarium.render_group(&ctx, "greet_email", "cs", None).unwrap();
+
+    println!("\nEnglish");
+    println!("=======\n");
+    rendered_group_en.iter().for_each(|(member_key, content)| println!("{}: {}", member_key, content));
+
+    println!("\nCzech");
+    println!("=====\n");
+    rendered_group_cs.iter().for_each(|(member_key, content)| println!("{}: {}", member_key, content));
 }
